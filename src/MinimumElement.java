5794e6929a4dc8613d96c2f7ac164901c57ea1f9
MVBALA <43174220+MVBALA@users.noreply.github.com>
2022-09-19 19:41:11 +0530

    Update MinimumElement.java

--- src/MinimumElement.java
+++ src/MinimumElement.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class MinimumElement {
-}
+
+
+//it is udemy Array Minumum challenge.
+
+//import java.util.Scanner;
+//
+//public class MinimumElement {
+//
+//    private static int readInteger(){
+//       Scanner input=new Scanner(System.in);
+//        int numberOfaElements=input.nextInt();
+//        input.close();
+//        return numberOfaElements;
+//
+//    }
+//    private static int[] readElements(int readInteger){
+//        Scanner input=new Scanner(System.in);
+//        int[] array=new int[readInteger];
+//        for (int i=0;i<array.length;i++){
+//            array[i]=input.nextInt();
+//            input.nextLine();
+//        }
+//        input.close();
+//        return array;
+//    }
+//    private static int findMin(int[] array){
+//        int min=Integer.MAX_VALUE;
+//        for (int i=0;i<array.length;i++){
+//            min=array[i]<min?array[i]:min;
+//        }
+//        return min;
+//    }
+//
+//}
