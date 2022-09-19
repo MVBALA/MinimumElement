6f10faf5b7efe6a239814134c086e1b2978111da
MVBALA <43174220+MVBALA@users.noreply.github.com>
2022-09-19 19:40:45 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter count:");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+//            int[] returnedArray = readIntegers(count);
+        int returnedMin = findMin(readIntegers(count));
+
+        System.out.println("min = " + returnedMin);
+
+    }
+
+    private static int[] readIntegers(int count) {
+
+        int[] array = new int[count];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Enter a number:");
+            array[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+        return array;
+    }
+
+    private static int findMin(int[] array) {
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            min = array[i] < min ? array[i] : min;
+
+        }
+
+        return min;
     }
 }
